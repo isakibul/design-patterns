ba07b5dd05c03570f55f6419973e51cb3e73d1a2
isakibul <isakibul108@gmail.com>
2025-07-18 23:12:46 +0600

    add: constructor

---
+++ constructor.js
@@ -0,0 +1,15 @@
+class person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+}
+
+const examplePerson = new person("Alice", 30);
+examplePerson.sayHello();
